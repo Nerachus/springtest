c290df8f6d9970fbba141cb2219ee63af70c907c
Janik Lüthi <nerachus@gmail.com>
2018-04-12 18:43:39 +0200

    security (disabled)

---
+++ src/main/kotlin/rest/WebSecurityConfig.kt
@@ -0,0 +1,38 @@
+package rest
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().permitAll()//.authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+    }
+
+    @Bean
+    public override fun userDetailsService(): UserDetailsService {
+        val user = User("user", "password", listOf(SimpleGrantedAuthority("USER")))
+
+        return InMemoryUserDetailsManager(listOf(user))
+    }
+}
